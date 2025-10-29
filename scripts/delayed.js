849f9d9d99c6f75846524425a677f53b7c993b5f
abhi00123 <abhi20.9.2002@gmail.com>
2025-10-30 00:14:31 +0530

    script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,15 @@
 // add delayed functionality here
+(() => {
+  console.log('Injecting Adobe Launch (Development) script...');
+
+  const script = document.createElement('script');
+  script.src = 'https://assets.adobedtm.com/77e0831c0028/5835c188116e/launch-2f37ff2f2ecd-development.min.js';
+  script.async = true;
+  script.type = 'text/javascript';
+
+  script.onload = () => console.log('✅ Adobe Launch Dev Script Loaded');
+  script.onerror = () => console.error('❌ Failed to load Adobe Launch Dev Script');
+
+  document.head.appendChild(script);
+})();
+
